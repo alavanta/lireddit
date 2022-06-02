ab98e73a272cbe5655c1f87315c99447d618362c
Dhieo Deva Alavanta <alavanta.deva@gmail.com>
2022-06-02 23:22:03 +0700

    disabled migration

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,7 +29,7 @@ export const dataSource = new DataSource({
 
 const main = async () => {
   const conn = await dataSource.initialize();
-  conn.runMigrations();
+  // conn.runMigrations();
 
   const app = express();
 
