253696e9a0cfeb42b655b9d8e3b890fc756459e0
Dhieo Deva Alavanta <alavanta.deva@gmail.com>
2022-06-05 20:04:56 +0700

    invalidate query: posts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -43,8 +43,8 @@ const Index = () => {
             ({ title, textSnippet, id, creator: { username } }) => (
               <Box key={id} p={5} shadow='md' borderWidth='1px'>
                 <Heading fontSize='xl'>{title}</Heading>
+                <Text>posted by {username}</Text>
                 <Text mt={4}>{textSnippet}</Text>
-                <Text my={8}>{username}</Text>
               </Box>
             )
           )}
