03081901ae61e5c5749406112237b1b7d610b955
Dhieo Deva Alavanta <alavanta.deva@gmail.com>
2022-06-07 22:35:46 +0700

    change updoot/downdoot logic

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -50,17 +50,57 @@ export class PostResolver {
     const { userId } = req.session;
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
-    await dataSource.query(
-      `
-     START TRANSACTION;
-      insert into updoot ("userId","postId","value")
-      values (${userId},${postId},${realValue});
-      update post 
-      set points = points + ${realValue}
-      where id = ${postId};
-     COMMIT;
-    `
-    );
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
+    if (updoot && updoot.value !== realValue) {
+      dataSource.transaction(async (tm) => {
+        await tm.query(
+          `
+            update updoot
+            set value = $1
+            where "postId" = $2 and "userId" = $3
+          `,
+          [realValue, postId, userId]
+        );
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2
+        `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      await dataSource.transaction(async (tm) => {
+        await tm.query(
+          `
+          insert into updoot ("userId","postId","value")
+          values ($1,$2,$3);
+         `,
+          [userId, postId, realValue]
+        );
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2
+        `,
+          [realValue, postId]
+        );
+      });
+    }
+
+    // await dataSource.query(
+    //   `
+    //  START TRANSACTION;
+    //   insert into updoot ("userId","postId","value")
+    //   values (${userId},${postId},${realValue});
+    //   update post
+    //   set points = points + ${realValue}
+    //   where id = ${postId};
+    //  COMMIT;
+    // `
+    // );
     return true;
   }
 
